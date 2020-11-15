ad9f8a48b7c60eb31b2a48880bfe0522e2b2ca73
Seb <sdieunidou@users.noreply.github.com>
2020-11-15 13:02:15 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ module.exports = {
               client.user.setActivity(`${client.guilds.cache.size} serveur(s)`, { type: 'LISTENING' }), 5000 * 60) 
         };
 
-        updateActivity(); // le bot est 'ready', on initialise le message d'activité. Ensuite, le setInterval() permettra de le rafraîchir le message toutes les 5 minutes
+        updateActivity(); // le bot est 'ready', on initialise le message d'activité. Ensuite, le setInterval() permettra de rafraîchir le message toutes les 5 minutes
 
         client.setInterval( () => updateActivity(), 5000 * 60 ); // toutes les 5 minutes après le lancement du bot, le message de présence sera mis à jour, et le compteur rafraîchi.
     }
